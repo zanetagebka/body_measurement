0827d4f38d0be053057afcff5f91d5e829f30943
zanetagebka <zanetagebka@gmail.com>
2025-03-07 21:30:34 +0100

    Add Polish translations for model fields

--- measurements/models.py
+++ measurements/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 class Measurement(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    waist = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    hips = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    chest = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    thigh = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    calf = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    forearm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    date = models.DateField(verbose_name=_('Data'))
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Waga'))
+    waist = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Talia'))
+    hips = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Biodra'))
+    chest = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Klatka piersiowa'))
+    thigh = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Udo'))
+    calf = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Łydka'))
+    forearm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_('Przedramię'))
 
     class Meta:
         ordering = ['-date']
